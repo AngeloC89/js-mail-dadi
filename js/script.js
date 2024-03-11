26fd6f75f11e1f9fa7403a8c4ec185b2c73b1b59
AngeloC89 <Angelociulla89@gmail.com>
2024-03-11 16:13:07 +0100

    the program work - add javascript with variable and "if" inside button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
+//Creo delle variabili per ottenere dall'utente delle scelte
+
+let userChoice = document.getElementById('userchoice');
+let userNumber = document.getElementById('numberchoice');
+console.log(userchoice, userNumber);
+
+//inserisco la funzione random che genera numeri a caso.
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
 
-let userchoice = document.getElementById('userChoice');
-let usernumber = parseInt((''))
-console.log(userchoice, usernumber);
+//identifico il bottone dentro una variabile tramite la sua classe
+let elButton = document.querySelector('.btn-primary');
+
+
+//dico al bottone quello che deve fare non appena lo clikkiamo
+elButton.addEventListener('click', function () {
+    let pcnumber = getRndInteger(1, 6);
+    console.log(pcnumber);
+    let sum = parseInt(userNumber.value) + pcnumber;
+    console.log(sum);
+    let userChoiceV = userChoice.value;
+
+
+    if ((sum % 2 === 0 && userChoiceV === 'even') || (sum % 2 !== 0 && userChoiceV === 'odd')) {
+        console.log('Hai vinto!');
+    } else {
+        console.log('Hai perso!');
+    }});
 
 
 
