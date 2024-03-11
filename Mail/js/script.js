bb1048a1b33af8c8c059d175a393e288a1e5f393
AngeloC89 <Angelociulla89@gmail.com>
2024-03-11 17:14:59 +0100

    init mail page and javascript

---
+++ Mail/js/script.js
@@ -0,0 +1,39 @@
+let userMail = document.getElementById ('emailsearch')
+let library = [
+    'angelociulla89gmail.com',
+    'pincopallino99@htomail.com',
+    'cicciopasticcio09@yahoo.com',
+    'runnindog-33@live.it',
+    'moanapozzi-223@libero.it'
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
